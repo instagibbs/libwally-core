9097a0472e5d3cdc068378e7731fc84f8eabe5d9
Gregory Sanders <gsanders87@gmail.com>
2019-07-16 13:12:41 -0700

    Add test for prefix-based mnemonics

--- src/test/test_mnemonic.py
+++ src/test/test_mnemonic.py
@@ -8,6 +8,7 @@ class MnemonicTests(unittest.TestCase):
     def setUp(self):
         if self.wl is None and wordlist_init is not None:
             self.words_list, words = load_words('english')
+            self.words_prefix_list = [x[:4] for x in self.words_list]
             self.wl = wordlist_init(utf8(words))
 
 
@@ -20,17 +21,27 @@ class MnemonicTests(unittest.TestCase):
         self.assertEqual(LEN, PHRASES_BYTES)
 
         buf = create_string_buffer(LEN)
+        prefix_buf = create_string_buffer(LEN)
 
         # Test round tripping
         for i in range(len(self.words_list) - PHRASES):
             phrase = utf8(' '.join(self.words_list[i : i + PHRASES]))
+            prefix_phrase = utf8(' '.join(self.words_prefix_list[i : i + PHRASES]))
 
             ret, written = mnemonic_to_bytes(self.wl, phrase, buf, LEN)
-            self.assertEqual(ret, 0)
-            self.assertEqual(written, PHRASES_BYTES)
-            generated = mnemonic_from_bytes(self.wl, buf, LEN)
-            self.assertEqual(phrase, generated)
 
+            prefix_ret, prefix_written = mnemonic_prefix_to_bytes(self.wl, prefix_phrase, prefix_buf, LEN)
+
+            if prefix_ret != 0:
+                continue
+            self.assertEqual(ret, 0)
+            self.assertEqual(prefix_ret, 0)
+            self.assertEqual(written, PHRASES_BYTES)
+            self.assertEqual(prefix_written, PHRASES_BYTES)
+            generated = mnemonic_from_bytes(self.wl, buf, LEN)
+            prefix_generated = mnemonic_from_bytes(self.wl, prefix_buf, LEN)
+            self.assertEqual(phrase, generated)
+            self.assertEqual(phrase, prefix_generated)
 
 if __name__ == '__main__':
     unittest.main()
